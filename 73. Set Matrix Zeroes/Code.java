f1293c8faf136b3c42c6c141c7803f0621bc471c
Prudhvi-Teja <87839700+Prudhvi-Teja@users.noreply.github.com>
2023-06-04 20:39:33 +0530

    Update Code.java

--- 73. Set Matrix Zeroes/Code.java
+++ 73. Set Matrix Zeroes/Code.java
@@ -1,21 +1,26 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
         
+        // Matrix row and length
         int m = matrix.length;
         int n = matrix[0].length;
         
+        // Taking these two fields whether first row and colums contains any zeros 
         int x = 1;
         int y = 1;
         
-      
+        // if first colums contains zeros initialize X field to zero
         for(int j = 0; j < n ; j++ ){
             if(matrix[0][j] == 0) x = 0;
         }
         
+        // if first row contains zeros initialize Y field to zero
         for(int i= 0; i<m ; i++){
             if(matrix[i][0] == 0) y = 0;
         }
         
+        
+        // check the rows from 1 to last and update the position in first row and columns if it contains zeros 
         for(int i = 1; i< m ; i++){
             
             for(int j = 1 ; j < n ; j++){
@@ -31,7 +36,7 @@ class Solution {
         
         
         
-        
+        // if it contains zero then make change in its particular row and colums
         for( int j = 1 ; j < n ; j++){
             if(matrix[0][j]==0){
                 for(int i = 0; i < m ; i++){
@@ -50,6 +55,7 @@ class Solution {
             
         }
         
+        // finally if x and y contains zeros and make these position also zeros
         if(x == 0 ){
             for(int j = 0; j < n ; j++)
                 matrix[0][j] = 0;
