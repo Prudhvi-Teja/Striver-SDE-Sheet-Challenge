f07f051245ea41ccc51f75ed2453327730c40a94
Prudhvi-Teja <87839700+Prudhvi-Teja@users.noreply.github.com>
2023-06-05 01:03:15 +0530

    Update Solution.java

--- Day-1/Set 0,1, 2 in sorted /Solution.java
+++ Day-1/Set 0,1, 2 in sorted /Solution.java
@@ -1,1 +1,35 @@
 
+import java.util.* ;
+import java.io.*; 
+public class Solution 
+{
+    public static void sort012(int[] arr)
+    {
+        //Write your code here
+
+        int p0=0, p1=0, p2=arr.length-1;
+
+        while(p2>=p1){
+            
+            if(arr[p1]==0){
+                int temp = arr[p1];
+                arr[p1] = arr[p0];
+                arr[p0] = temp;
+                p0++;
+                p1++;
+            }
+            else if(arr[p1]==1){
+                p1++;
+            }
+            else{
+                int temp = arr[p2];
+                arr[p2] = arr[p1];
+                arr[p1] = temp;
+                p2--;
+            }
+
+        }
+
+
+    }
+}
